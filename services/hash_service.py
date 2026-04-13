14ca115d6402ff7878b7480f220e76ee101508e0
Adrija <143275613+adrxjaa@users.noreply.github.com>
2026-04-13 20:05:53 +0530

    Delete services directory

--- services/hash_service.py
+++
@@ -1,4 +0,0 @@
-import hashlib
-
-def generate_image_hash(image_bytes):
-    return hashlib.sha256(image_bytes).hexdigest()
--- services/ocr_service.py
+++
@@ -1,7 +0,0 @@
-import pytesseract
-from PIL import Image
-import io
-
-def extract_text(image_bytes):
-    image = Image.open(io.BytesIO(image_bytes))
-    return pytesseract.image_to_string(image)
